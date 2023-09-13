57d90af12ff44533381fa220a49e679081efb570
Wener Rodrigo <wener19.wr@gmail.com>
2023-09-13 16:41:02 -0300

    refatorando o código , criado melhorias entre pastas

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView } from 'react-native';
+import { StatusBar, SafeAreaView, View } from 'react-native';
 import Cesta from './src/screens/Cesta';
+import { Montserrat_400Regular,Montserrat_700Bold } from '@expo-google-fonts/montserrat';
+import { useFonts } from 'expo-font';
+import mock from './src/mocks/cesta';
+import AppLoading from 'expo-app-loading';
 
 export default function App() {
+
+  const [fontesCarregada] = useFonts({
+    "MontserratRegular": Montserrat_400Regular,
+    "MontserratBold": Montserrat_700Bold,
+  
+  });
+
+  if (!fontesCarregada) {
+    return <AppLoading />
+  }
+
   return (
     <SafeAreaView>
       <StatusBar />
-      <Cesta />
+      <Cesta {...mock}/>
     </SafeAreaView>
   );
 }
